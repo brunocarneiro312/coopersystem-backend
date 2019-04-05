1a0a8982e1fd1e869fea6074bdb181219179a59f
bruno.carneiro <bruno.carneiro@gmail.com>
2019-04-05 15:52:30 -0300

    model conta

---
+++ src/main/java/br/com/coopersystem/backendcoopersystem/model/Conta.java
@@ -0,0 +1,24 @@
+package br.com.coopersystem.backendcoopersystem.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "TB_CONTA")
+public class Conta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequence_conta")
+    @SequenceGenerator(name = "sequence_conta", sequenceName = "seq_conta")
+    @Column(name = "ID_CONTA")
+    private Long id;
+
+    @Column(name = "USUARIO", unique = true, nullable = false)
+    private String usuario;
+
+    @Column(name = "SENHA", nullable = false)
+    private String senha;
+
+}
