9c35951859c5f9123e05403636fe80da0848ba00
bruno.carneiro <bruno.carneiro@gmail.com>
2019-04-05 15:34:06 -0300

    primeiro commit

---
+++ src/main/java/br/com/coopersystem/backendcoopersystem/BackendCoopersystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.coopersystem.backendcoopersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCoopersystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendCoopersystemApplication.class, args);
+    }
+
+}
